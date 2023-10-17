dbc27430a3190e8f32ccf7179690c8b42c988998
Ndinne <144891852+Ndinne@users.noreply.github.com>
2023-10-17 12:15:11 +0200

    Update gau1.py

--- gau1.py
+++ gau1.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 response = requests.get(f"https://www.autotrader.co.za/cars-for-sale/gauteng/p-1?pagenumber=1&sortorder=Newest&priceoption=RetailPrice")
 home_page = BeautifulSoup(response.content, 'lxml')
 print(home_page)
