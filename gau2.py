85e5a62acef5f3eac8d4b007060c827c6dd576fc
Ndinne <144891852+Ndinne@users.noreply.github.com>
2024-07-25 14:10:21 +0200

    Update gau2.py

--- gau2.py
+++ gau2.py
@@ -52,7 +52,7 @@ def ingest_data(all_data):
         data_frame = pd.DataFrame(all_data)
         # Save DataFrame to CSV
         car_data_csv = data_frame.to_csv(encoding="utf-8", index=False)
-        sas_url = f"https://stautotrader.blob.core.windows.net/properties/AirbnbListings{start_time}.csv?sv=2021-10-04&ss=btqf&srt=sco&st=2023-10-17T07%3A39%3A17Z&se=2030-10-18T07%3A39%3A00Z&sp=rwdxftlacup&sig=%2BTFZttmuMZLkl%2Bq%2Bf2t%2FPNBSJkWUzw52PPp1sL9X8Wk%3D"
+        sas_url = f"https://stautotrader.blob.core.windows.net/properties/AirbnbListings2t{start_time}.csv?sv=2021-10-04&ss=btqf&srt=sco&st=2023-10-17T07%3A39%3A17Z&se=2030-10-18T07%3A39%3A00Z&sp=rwdxftlacup&sig=%2BTFZttmuMZLkl%2Bq%2Bf2t%2FPNBSJkWUzw52PPp1sL9X8Wk%3D"
         client = BlobClient.from_blob_url(sas_url)
         client.upload_blob(car_data_csv, overwrite=True) 
 
